cd557f3b2bda5a864646051c94450165806a01d3
shiv-ko <iscopre7@gmail.com>
2025-12-01 11:51:09 +0900

    chore: prisma環境の修正

--- apps/backend/src/prisma.service.ts
+++ apps/backend/src/prisma.service.ts
@@ -1,11 +1,26 @@
 import { Injectable, OnModuleInit } from "@nestjs/common";
 
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    if (!connectionString) {
+      throw new Error("DATABASE_URL is not defined");
+    }
+    const pool = new Pool({ connectionString });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment
+    const adapter = new PrismaPg(pool) as any;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
+    super({ adapter });
+  }
+
   async onModuleInit(): Promise<void> {
     // Note: $connect is inherited from PrismaClient
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$connect();
   }
 }
