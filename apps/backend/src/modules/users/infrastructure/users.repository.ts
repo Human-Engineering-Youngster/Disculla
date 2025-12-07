b2c62d7bcdcccd7833843e25e4bf8f87fdfeb325
kmjak <tymkoron1221@icloud.com>
2025-12-08 03:57:24 +0900

    refactor: 共通処理を関数化と例外処理の実装
    - PrismaのデータをEntityに変換する関数の実装
    - 例外処理とloggerの追加
    - Prismaから取得するUserの型を定義

--- apps/backend/src/modules/users/infrastructure/users.repository.ts
+++ apps/backend/src/modules/users/infrastructure/users.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException, Logger } from "@nestjs/common";
 
 import { PrismaService } from "src/modules/prisma/application/prisma.service";
 import { ClerkIdVo } from "src/modules/users/domain/clerk-id.vo";
@@ -8,43 +8,62 @@ import { IdVo } from "src/modules/users/domain/user-id.vo";
 import { NameVo } from "src/modules/users/domain/user-name.vo";
 import { User } from "src/modules/users/domain/user.entity";
 import { IUserRepository } from "src/modules/users/domain/user.repository.interface";
+import { PrismaUser } from "src/modules/users/infrastructure/prisma-user.type";
 
 @Injectable()
 export class UsersRepository implements IUserRepository {
+  private readonly logger = new Logger(UsersRepository.name);
+
   constructor(private readonly prismaService: PrismaService) {}
+
+  /**
+   * PrismaUserをUserエンティティに変換します。
+   * @param {PrismaUser} user - Prismaから取得したユーザーデータ
+   * @returns {User} 変換されたUserエンティティ
+   */
+  private toEntity(user: PrismaUser): User {
+    return User.reconstruct(
+      IdVo.of(user.id),
+      ClerkIdVo.of(user.clerkId),
+      NameVo.of(user.name),
+      AvatarUrlVo.of(user.avatarUrl)
+    );
+  }
 
   /**
    * 新しいユーザーを作成します。
    * @param {SaveUserVo} user - 保存するユーザーデータ
    * @returns {User} 作成されたUserエンティティ
+   * @throws InternalServerErrorException - 作成中にエラーが発生した場合にスローされます。
    */
   async create(user: SaveUserVo): Promise<User> {
-    return await this.prismaService.user
-      .create({
+    try {
+      const createdUser: PrismaUser = await this.prismaService.user.create({
         data: {
           clerkId: user.getClerkId().getValue(),
           name: user.getName().getValue(),
           avatarUrl: user.getAvatarUrl().getValue(),
         },
-      })
-      .then((prismaUser) => {
-        return User.reconstruct(
-          IdVo.of(prismaUser.id),
-          ClerkIdVo.of(prismaUser.clerkId),
-          NameVo.of(prismaUser.name),
-          AvatarUrlVo.of(prismaUser.avatarUrl)
-        );
       });
+
+      return this.toEntity(createdUser);
+    } catch (error) {
+      const errorMessage = "Failed to create user";
+      this.logger.error(errorMessage, error);
+
+      throw new InternalServerErrorException(errorMessage);
+    }
   }
 
   /**
    * 既存のユーザーを更新します。
    * @param {SaveUserVo} user - 更新するユーザーデータ
    * @returns {User} 更新されたUserエンティティ
+   * @throws InternalServerErrorException - 更新中にエラーが発生した場合にスローされます。
    */
   async update(user: SaveUserVo): Promise<User> {
-    return await this.prismaService.user
-      .update({
+    try {
+      const updatedUser = await this.prismaService.user.update({
         where: {
           clerkId: user.getClerkId().getValue(),
         },
@@ -52,14 +71,14 @@ export class UsersRepository implements IUserRepository {
           name: user.getName().getValue(),
           avatarUrl: user.getAvatarUrl().getValue(),
         },
-      })
-      .then((prismaUser) => {
-        return User.reconstruct(
-          IdVo.of(prismaUser.id),
-          ClerkIdVo.of(prismaUser.clerkId),
-          NameVo.of(prismaUser.name),
-          AvatarUrlVo.of(prismaUser.avatarUrl)
-        );
       });
+
+      return this.toEntity(updatedUser);
+    } catch (error) {
+      const errorMessage = "Failed to update user";
+      this.logger.error(errorMessage, error);
+
+      throw new InternalServerErrorException(errorMessage);
+    }
   }
 }
